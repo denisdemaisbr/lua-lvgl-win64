6fb2883fe4d08b6a9cc524d1d8c6b4967017b2f1
DENIS DOS SANTOS SILVA <denis@roo.com.br>
2022-12-04 17:27:36 -0300

    Add files via upload

---
+++ fb2.lua
@@ -0,0 +1,24 @@
+f = require 'fb'
+
+f.clear(0xffffff)
+
+for i=1, 5 do
+f.draw_rectangle( 10+i, 10+i, 100-i, 100-i, 0xff00ff );
+end
+
+f.draw_circle( 90, 90, 50, 0x00ff00 );
+
+for i=1, 25 do
+f.draw_line( 10, 10, 100+i, 100-i, 0xff0000 );
+f.draw_line( 10, 10, 100-i, 100+i, 0x0ffff0 );
+end
+
+
+
+while(true) do
+	if (not f.update()) then break; end
+	
+	f.sync();	
+end
+
+print(_VERSION)
